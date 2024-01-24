c05f52f4777ce1e354b27f99c588f407f1bfce96
Dev Royale <unityekeoba18@gmail.com>
2024-01-24 22:47:41 +0100

    Update Contact.js

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "./../components/Layout/Layout";
-import { BiMailSend, BiPhoneCall, BiSupport } from "react-icons/bi";
+import { BiMailSend, BiPhoneCall, BiSupport } from "react-icons";
 const Contact = () => {
   return (
     <Layout>
